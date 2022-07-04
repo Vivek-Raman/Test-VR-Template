1560fade7a4974ae1099caba512ceac1a7df88a2
Vivek Raman <vivek.raman@gdn-commerce.com>
2022-07-04 22:19:17 +0530

    Fix SceneScelection

--- Assets/Scripts/DebugUIManager.cs
+++ Assets/Scripts/DebugUIManager.cs
@@ -4,32 +4,26 @@ using UnityEngine.UI;
 
 public class DebugUIManager : MonoBehaviour
 {
-    private string selectedScene = "";
+    private string selectedScenePath = "";
     
     private void Start()
     {
-        int sceneCount = SceneManager.sceneCount;
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
         for (int i = 0; i < sceneCount; ++i)
         {
-            Scene scene = SceneManager.GetSceneAt(i);
-            DebugUIBuilder.instance.AddRadio(scene.name, "sceneList", (Toggle t) =>
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            DebugUIBuilder.instance.AddRadio(scenePath, "sceneList", (Toggle t) =>
             {
-                OnSceneSelectionChanged(scene.name, t.isOn);
+                if (!t.isOn) return;
+                selectedScenePath = scenePath;
             });
         }
 
         DebugUIBuilder.instance.AddButton("Load Scene", () =>
         {
-            if (SceneManager.GetSceneByName(selectedScene).name == selectedScene)
-            {
-                SceneManager.LoadScene(selectedScene);
-            }
+            SceneManager.LoadScene(SceneUtility.GetBuildIndexByScenePath(selectedScenePath));
         });
-    }
 
-    private void OnSceneSelectionChanged(string radioLabel, bool selected)
-    {
-        if (!selected) return;
-        selectedScene = radioLabel;
+        DebugUIBuilder.instance.Show();
     }
 }
